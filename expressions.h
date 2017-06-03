4818a8e9e462d469f9d9ac4128f5e21deecaf3cb
Christopher Hittner <chrishittner@gmail.com>
2017-06-03 00:05:41 -0400

    Evaluation Function Types
    These are meant to be a basis for developing evaluators.

---
+++ expressions.h
@@ -0,0 +1,26 @@
+#ifndef _EXPRESSIONS_H_
+#define _EXPRESSIONS_H_
+
+#include "structures.h"
+
+/**
+ * Defines ExpConstructor to be a function type for
+ * creating Exps.
+ *
+ * void** -> Exp
+ */
+typedef Exp (*ExpConstructor)(void**);
+
+/**
+ * Defines a type that evaluates an expression.
+ * Type: {Exp, Env} -> void*
+ */
+typedef ExpVal (*ExpEvaluator)(Exp, Env);
+
+/**
+ * Evaluates a given expression.
+ * Type: ExpVal
+ */
+ExpVal value_of(Exp, Env);
+
+#endif
